31184e0f72a37d7a13b2a196c8163bab9c2938ad
arsam <arsamshokrian@gmail.com>
2020-06-01 18:49:59 +0200

    Updates

---
+++ precache-manifest.12aad9220debdb36161446fde239c954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a04fc07e3a3c262fd7df3b308e93301",
+    "url": "/arsamshokrian/arsamshokrian.git/index.html"
+  },
+  {
+    "revision": "8e0f487874e4fed1e84a",
+    "url": "/arsamshokrian/arsamshokrian.git/static/css/main.2f597592.chunk.css"
+  },
+  {
+    "revision": "70764944418ec6c6654d",
+    "url": "/arsamshokrian/arsamshokrian.git/static/js/2.df5e1e46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/arsamshokrian/arsamshokrian.git/static/js/2.df5e1e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0f487874e4fed1e84a",
+    "url": "/arsamshokrian/arsamshokrian.git/static/js/main.10d7531e.chunk.js"
+  },
+  {
+    "revision": "ae6f0677406fe46596d7",
+    "url": "/arsamshokrian/arsamshokrian.git/static/js/runtime-main.b65a8315.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arsamshokrian.github.io/precache-manifest.183a7569d311ec5576a3ec3aa9aa4a6a.js"
+  "/arsamshokrian/arsamshokrian.git/precache-manifest.12aad9220debdb36161446fde239c954.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arsamshokrian.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arsamshokrian/arsamshokrian.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
