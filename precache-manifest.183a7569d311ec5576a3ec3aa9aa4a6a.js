24b8eb82e70db96fd7616325d18da1febf3097bf
arsam <arsamshokrian@gmail.com>
2020-06-01 18:41:56 +0200

    Updates

---
+++ precache-manifest.183a7569d311ec5576a3ec3aa9aa4a6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7742ea36fbec00a01bd5cf8fee3e90",
+    "url": "/arsamshokrian.github.io/index.html"
+  },
+  {
+    "revision": "b8533ccf0dd4cd255555",
+    "url": "/arsamshokrian.github.io/static/css/main.2f597592.chunk.css"
+  },
+  {
+    "revision": "70764944418ec6c6654d",
+    "url": "/arsamshokrian.github.io/static/js/2.df5e1e46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/arsamshokrian.github.io/static/js/2.df5e1e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8533ccf0dd4cd255555",
+    "url": "/arsamshokrian.github.io/static/js/main.aa9b8e15.chunk.js"
+  },
+  {
+    "revision": "500e7bdf269ba103f421",
+    "url": "/arsamshokrian.github.io/static/js/runtime-main.5dfe7398.js"
+  }
+]);
